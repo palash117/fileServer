ac996f0896e7b2847691fb2c071f3ccf36fb88ae
palash117 <palash2392@gmail.com>
2020-04-20 16:38:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 	"fmt"
 )
 
-func init(){
+func init() {
 	fmt.Println("init done")
 }
 
-func main(){
+func main() {
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
 	fmt.Println("main done")
 	dao.ReadyDb()
 	router.Start()
