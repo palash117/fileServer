067c1c3a6397c56330aa5c57ccc4130e06ecb133
palash117 <palash2392@gmail.com>
2020-04-20 16:39:15 +0530

    Update baseRouter.go

--- router/baseRouter.go
+++ router/baseRouter.go
@@ -7,39 +7,51 @@ import (
 	"os"
 )
 
-var PORT = fmt.Sprint(":",os.Getenv("FS_PORT"))
-const(
-	BASE_PATH = "/fs"
-	ADD_FILE_PATH = "/addFile"
-	HEALTH = "/health"
-	UPLOAD_FILE_PATH = "/uploadFile"
-	GET_PAGINATED_PATH = "/getPaginated"
-	DOWNLOAD_FILE_BY_ID ="/downloadFileById"
+var PORT = fmt.Sprint(":", os.Getenv("FS_PORT"))
+
+const (
+	BASE_PATH           = "/fs"
+	ADD_FILE_PATH       = "/addFile"
+	HEALTH              = "/health"
+	UPLOAD_FILE_PATH    = "/uploadFile"
+	GET_PAGINATED_PATH  = "/getPaginated"
+	DOWNLOAD_FILE_BY_ID = "/downloadFileById"
+	HOME                = "/home"
+	SERVE               = "/serve/"
 )
 
-type handler struct{
-	path string
-	handlerFunc func( http.ResponseWriter,  * http.Request)
+type handler struct {
+	path        string
+	handlerFunc func(http.ResponseWriter, *http.Request)
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request){
+func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	h.handlerFunc(w, req)
 }
 
-func Start(){
+func Start() {
 	fmt.Println("started")
 
-	http.Handle(BASE_PATH+ ADD_FILE_PATH, handler{ADD_FILE_PATH, controller.AddFile} )
+	http.Handle(BASE_PATH+ADD_FILE_PATH, handler{ADD_FILE_PATH, controller.AddFile})
 
-	http.Handle(BASE_PATH + HEALTH,  handler{HEALTH, controller.Health})
+	http.Handle(BASE_PATH+HEALTH, handler{HEALTH, controller.Health})
 
-	http.Handle(BASE_PATH + UPLOAD_FILE_PATH,  handler{UPLOAD_FILE_PATH, controller.UploadFile})
+	http.Handle(BASE_PATH+UPLOAD_FILE_PATH, handler{UPLOAD_FILE_PATH, controller.UploadFile})
 
-	http.Handle(BASE_PATH + GET_PAGINATED_PATH,  handler{GET_PAGINATED_PATH, controller.GetPaginatedItems})
+	http.Handle(BASE_PATH+GET_PAGINATED_PATH, handler{GET_PAGINATED_PATH, controller.GetPaginatedItems})
 
-	http.Handle(BASE_PATH + DOWNLOAD_FILE_BY_ID,  handler{DOWNLOAD_FILE_BY_ID, controller.DownloadFileById})
+	http.Handle(BASE_PATH+DOWNLOAD_FILE_BY_ID, handler{DOWNLOAD_FILE_BY_ID, controller.DownloadFileById})
 
-	http.ListenAndServe(PORT, nil)
+	http.Handle(SERVE, http.StripPrefix(SERVE, http.FileServer(http.Dir("./web"))))
+	path, errx := os.Getwd()
+	if errx != nil {
+		fmt.Println(errx)
+	}
+	fmt.Println(path)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Println("started 2 ")
 }
