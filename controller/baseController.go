bca88f291c09c7285be02bb719ac2291fb387671
palash117 <palash2392@gmail.com>
2020-04-20 16:37:33 +0530

    Update baseController.go

--- controller/baseController.go
+++ controller/baseController.go
@@ -2,51 +2,49 @@ package controller
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 	"fileServer/dao"
 	"fileServer/dto"
 	"fileServer/models"
 	"fileServer/service"
+	"fmt"
+	"io"
+	"net/http"
 )
 
-const(
+const (
 	BASE_FILE_PATH = "/Users/palash.sarkar/fsdownloads"
 )
-func AddFile(w http.ResponseWriter, req *http.Request)  {
+
+func AddFile(w http.ResponseWriter, req *http.Request) {
 	msg := "add file api"
-	w.Header().Add("ContentType","Application/Json")
+	w.Header().Add("ContentType", "Application/Json")
 	w.WriteHeader(http.StatusOK)
-	dto :=dto.BaseDto{msg}
-	jsonDto,_ := json.Marshal(dto)
+	dto := dto.BaseDto{msg}
+	jsonDto, _ := json.Marshal(dto)
 	w.Write(jsonDto)
 
 }
 
-
-func UploadFile(w http.ResponseWriter, r *http.Request){
+func UploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("File Upload Endpoint Hit")
 
-	service.UploadFileAndsaveToDb(w,r)
-
+	service.UploadFileAndsaveToDb(w, r)
 
 }
-func Health (w http.ResponseWriter, req *http.Request){
+func Health(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "file server is up")
 }
 
-
-func GetPaginatedItems(w http.ResponseWriter, r * http.Request){
-	obj:= new(models.Item)
+func GetPaginatedItems(w http.ResponseWriter, r *http.Request) {
+	obj := new(models.Item)
 	fmt.Println(obj)
-	data:= dao.GetItemsPaginated(0,0)
-	w.Header().Add("ContentType","Application/Json")
+	data := dao.GetItemsPaginated(0, 0)
+	w.Header().Add("ContentType", "Application/Json")
 	w.WriteHeader(http.StatusOK)
-	jsonDto,_ := json.Marshal(data)
+	jsonDto, _ := json.Marshal(data)
 	w.Write(jsonDto)
 }
 
-func DownloadFileById(w http.ResponseWriter, r *http.Request){
-	service.DownloadFileById(w , r)
+func DownloadFileById(w http.ResponseWriter, r *http.Request) {
+	service.DownloadFileById(w, r)
 }
