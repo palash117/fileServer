9bf4ebb5bb2f7a59addc4484dbe72b676f4159c7
palash117 <palash2392@gmail.com>
2020-05-24 00:19:33 +0530

    build sh file

---
+++ build.sh
@@ -0,0 +1,24 @@
+cd ~/dev/fileserver
+echo "MOVING TO FILESERVER CODE DIRECTORY\n"
+
+git pull
+echo "PULLING LATEST CODE CHANGES\n"
+
+docker kill fsa
+docker container rm fsa
+echo "KILLING LAST KNOWN FILESERVER CONTAINER\n"
+
+docker rmi fsa
+docker build . | tee  /tmp/dockerbuild.txt
+echo "BUILDING LATEST DOCKER IMAGE"
+
+temp=$(tail -1 /tmp/dockerbuild.txt)
+IFS=' '
+read -ra ADDR <<< "$temp"
+for i in "${ADDR[@]}"; do
+	dockerImageId="$i"
+done
+echo "$dockerImageId"
+
+echo "RUNNING DOCKER IMAGE IN HEADLESS MODE"
+docker run -d -p 9010:9010 --name fsa -v /media/pi/sgt/fileserver:/go/src/fileServer/fileServer ${dockerImageId}
